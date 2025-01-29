5f50fd9c10b86a3f0f707e12a2497b4922f7113b
ASireix <tvv3show@gmail.com>
2025-01-29 19:33:08 +0100

    {ADD} - Model classes from JDM API

---
+++ APIRequest/Models/RelationRet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BotJDM.APIRequest.Models
+{
+    public class RelationRet
+    {
+        [JsonPropertyName("nodes")]
+        public List<Node> nodes { get; set; } = new();
+
+        [JsonPropertyName("relations")]
+        public List<Relation> relations { get; set; } = new();
+    }
+}
